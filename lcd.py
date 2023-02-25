1c6a5d69fd7f9eeeb46a9e623f5a5a61631d0545
wilchis <3491130+willchis@users.noreply.github.com>
2023-02-25 16:52:13 -0700

    fix typo in script

--- lcd.py
+++ lcd.py
@@ -29,8 +29,8 @@ def loop():
 
 def destroy():
     lcd.clear()
-    PCF8574_address = 0x27
 
+PCF8574_address = 0x27
 # I2C address of the PCF8574 chip.
 PCF8574A_address = 0x3F # I2C address of the PCF8574A chip.
 # Create PCF8574 GPIO adapter.
@@ -42,7 +42,7 @@ except:
     except:
         print ('I2C Address Error !')
         exit(1)
-        
+
 # Create LCD, passing in MCP GPIO adapter.
 lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
 
