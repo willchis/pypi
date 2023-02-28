cf445c43f3a68eed9987e258b8f7b3142ca89dc3
wilchis <3491130+willchis@users.noreply.github.com>
2023-02-27 22:43:56 -0700

    loop and destroy

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from lcd import lcd
-from weather import weather
+import datetime
 import json
+from lcd import lcd
+from time import sleep
+from weather import weather
+
+def get_time_now():    
+    return datetime.now().strftime('%H:%M:%S')
 
 if __name__ == '__main__':
     print ('Program is starting ... ')
-    #try:
     weather = weather()
     weather.request_data()
-    temperature = weather.tempF()
+    temperature = "Current: " + round(weather.tempF()) + "F"
     print(temperature)
     print(json.dumps(weather.data))
-    #except KeyboardInterrupt:
 
-       # destroy()
     lcd = lcd()
-    lcd.write(str(temperature) + "F", "current temp")
+    try:
+        while True:
+            lcd.write(temperature, "current temp", get_time_now())
+            sleep(1)
+    except KeyboardInterrupt:
+        lcd.destroy()
