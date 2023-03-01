0a17074ba23e7b3e6d3c2d67c10299674bb19a25
wilchis <3491130+willchis@users.noreply.github.com>
2023-02-28 22:26:59 -0700

    add timezones and cycling

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from datetime import datetime
 import json
-from lcd import lcd
+#from lcd import lcd
 from time import sleep
 from weather import weather
+from zoneinfo import ZoneInfo
 
-def get_time_now():    
-    return datetime.now().strftime('%H:%M:%S')
+PST = ZoneInfo("America/Los_Angeles")
+UK = ZoneInfo("Europe/Belfast")
+INDIA = ZoneInfo("Asia/Kolkata")
+
+def get_time_now(tz=None):
+    now = datetime.now()
+    
+    if (tz is not None):
+        now = now.astimezone(tz)
+    return now.strftime('%H:%M:%S')
 
 if __name__ == '__main__':
     print ('Program is starting ... ')
@@ -15,10 +24,22 @@ if __name__ == '__main__':
     print(temperature)
     print(json.dumps(weather.data))
 
-    lcd = lcd()
+    #lcd = lcd()
     try:
+        i = 0
+        duration = 3
+        times = {
+            "Local": None,
+            "UK": UK,
+            "PST": PST
+        }
+        times_index = list(times)
         while True:
-            lcd.write(temperature, get_time_now())
+            #lcd.write(temperature, get_time_now())
+            print(get_time_now(times[times_index[i]]) + " " + times_index[i])
             sleep(1)
+            i += 1
+            if (i >= len(times_index)):
+                i = 1
     except KeyboardInterrupt:
         lcd.destroy()
