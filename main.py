cabcd4b5d93c4c1e655eb61d60f21384742d1099
wilchis <3491130+willchis@users.noreply.github.com>
2023-03-02 22:45:39 -0700

    add stock price requests

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 import json
 #from lcd import lcd
-import sched, time
+from stocks import stocks
+from time import sleep
 from weather import weather
 from zoneinfo import ZoneInfo
 
@@ -10,10 +11,14 @@ UK = ZoneInfo("Europe/Belfast")
 INDIA = ZoneInfo("Asia/Kolkata")
 
 weather = weather()
+stocks = stocks()
 
 def update_weather_data(): 
     weather.request_data()
     print(json.dumps(weather.data))
+
+def update_stock_data():
+    stocks.request_data()
 
 def get_time_now(tz=None):
     now = datetime.now()
@@ -25,6 +30,7 @@ def get_time_now(tz=None):
 if __name__ == '__main__':
     print ('Program is starting ... ')
     update_weather_data()
+    update_stock_data()
 
     #lcd = lcd()
     try:
@@ -42,13 +48,19 @@ if __name__ == '__main__':
             time_output = get_time_now(times[times_index[chosen_time]]) + " " + times_index[chosen_time]
             temperature = str(round(weather.tempF())) + "°F"
             humidity = str(weather.humidity()) + "%"
+            stock_price = "STEM: $" + str(stocks.price)
+
+            if (stocks.change is not None):
+                stock_price += " " + str(stocks.change) + "%"
 
             #lcd.write(temperature + " " + humidity, time_output)
             print(time_output)
-            time.sleep(1)
+            sleep(1)
             i += 1
+            # refresh api every 60s
             if (i % 60 == 0):
                 update_weather_data()
+                update_stock_data()
             if (i % duration == 0):
                 chosen_time += 1
             if (chosen_time >= len(times_index)):
---
+++ stocks.py
@@ -0,0 +1,23 @@
+import json
+import os
+import requests
+
+class stocks():
+    finnhub_api_url = "https://finnhub.io/api/v1/quote"
+    finnhub_env_key = "FINNHUBKEY"
+
+    def __init__(self):
+        if stocks.finnhub_env_key not in os.environ:
+            raise Exception("Finnhub API Key not set in environment var: " + stocks.finnhub_env_key)
+
+    def request_data(self):
+        params = dict (
+                symbol="STEM",
+                token=os.environ[stocks.finnhub_env_key]
+            )
+        response = requests.get(url=stocks.finnhub_api_url, params=params)
+        response.raise_for_status()
+        json_response = response.json()
+        self.price = json_response["c"] if "c" in json_response else None
+        self.change = json_response["d"] if "d" in json_response else None
+        print("STEM price: " + str(self.price))
